5af91823e3e7a73a092206af7bf425c45bef4317
Alex Cortez <acim650@gmail.com>
2022-04-13 14:42:58 -0700

    formatting learned

--- target/debug/deps/Rust_Sandbox-94b88add72850792.d
+++ target/debug/deps/Rust_Sandbox-94b88add72850792.d
@@ -1,5 +1,6 @@
-/Users/l/Rust_Sandbox/target/debug/deps/Rust_Sandbox-94b88add72850792: src/main.rs
+/Users/l/Rust_Sandbox/target/debug/deps/Rust_Sandbox-94b88add72850792: src/main.rs src/print.rs
 
-/Users/l/Rust_Sandbox/target/debug/deps/Rust_Sandbox-94b88add72850792.d: src/main.rs
+/Users/l/Rust_Sandbox/target/debug/deps/Rust_Sandbox-94b88add72850792.d: src/main.rs src/print.rs
 
 src/main.rs:
+src/print.rs:
