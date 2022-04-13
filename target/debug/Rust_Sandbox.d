9986e46ed236d435f89e7c96ab9182838e3bcbf7
Alex Cortez <acim650@gmail.com>
2022-04-13 13:58:35 -0700

    first commit

---
+++ target/debug/Rust_Sandbox.d
@@ -0,0 +1,1 @@
+/Users/l/Rust_Sandbox/target/debug/Rust_Sandbox: /Users/l/Rust_Sandbox/src/main.rs
---
+++ target/debug/deps/Rust_Sandbox-94b88add72850792.d
@@ -0,0 +1,5 @@
+/Users/l/Rust_Sandbox/target/debug/deps/Rust_Sandbox-94b88add72850792: src/main.rs
+
+/Users/l/Rust_Sandbox/target/debug/deps/Rust_Sandbox-94b88add72850792.d: src/main.rs
+
+src/main.rs:
